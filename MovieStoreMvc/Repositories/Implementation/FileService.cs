079f5887e7f4f5223cf9b6e6af5cf5f771c99d13
Thawatchai Sangthep <94940261+MickBerryZ@users.noreply.github.com>
2023-04-05 12:00:06 +0700

    Add project files.

---
+++ MovieStoreMvc/Repositories/Implementation/FileService.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using MovieStoreMvc.Repositories.Abstract;
+
+namespace MovieStoreMvc.Repositories.Implementation
+{
+    public class FileService : IFileService
+    {
+        private readonly IWebHostEnvironment environment;
+        public FileService(IWebHostEnvironment env)
+        {
+            this.environment = env;
+        }
+
+        public Tuple<int, string> SaveImage(IFormFile imageFile)
+        {
+            try
+            {
+                var wwwPath = this.environment.WebRootPath;
+                var path = Path.Combine(wwwPath, "Uploads");
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+
+                // Check the allowed extensitions
+                var ext = Path.GetExtension(imageFile.FileName);
+                var allowedExtensions = new string[] { ".jpg", ".png", ".jpeg" };
+                if (!allowedExtensions.Contains(ext))
+                {
+                    string msg = string.Format("Only {0} extenstion are allowed", string.Join(",", allowedExtensions));
+                    return new Tuple<int, string>(0, msg);
+                }
+                string uniqueString = Guid.NewGuid().ToString();
+                // we are trying to create a unique filename here
+                var newFileName = uniqueString + ext;
+                var FileWithPath = Path.Combine(path, newFileName);
+                var stream = new FileStream(FileWithPath, FileMode.Create);
+                imageFile.CopyTo(stream);
+                stream.Close();
+                return new Tuple<int, string>(1, newFileName);
+            }
+            catch (Exception ex) 
+            {
+                return new Tuple<int, string>(0, "Error has occured");
+            }
+        }
+
+        public bool DeleteImage(string imageFileName)
+        {
+            try
+            {
+                var wwwPath = this.environment.WebRootPath;
+                var path = Path.Combine(wwwPath, "Uploads\\", imageFileName);
+                if (System.IO.File.Exists(path))
+                {
+                    System.IO.File.Delete(path);
+                    return true;
+                }
+                return false;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+    }
+}
